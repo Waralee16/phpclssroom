f90802af4a59d0d7811be7b2f36cf70d86d31294
Waralee16 <72967096+Waralee16@users.noreply.github.com>
2021-12-21 09:56:33 +0700

    Update province.php

--- province.php
+++ province.php
@@ -2,9 +2,18 @@
 
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
- 
-  foreach($data as $key=>$val){
-    echo $val->province."<br>";
-}
   
+  echo "<table border='1'>";
+  echo "<tr><td>ลำดับ</td><td>จังหวัด</td></tr>";
+  foreach($data as $key=>$val){
+    echo "<tr>";
+    echo "<td>";
+    echo $key;
+    echo "</td>";
+    echo "<td>";
+    echo $val->province;
+    echo "</td>";
+    echo "</tr>";
+  }
+  echo "</table>"; 
 ?>
